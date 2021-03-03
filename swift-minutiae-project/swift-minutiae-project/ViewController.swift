e69ebbaa11ab243aec4de9428f4c9e797719a310
Vanessa Primetzhofer <vaniprimetz@gmail.com>
2021-03-03 07:54:06 -0800

    update first page design setup

--- swift-minutiae-project/swift-minutiae-project/ViewController.swift
+++ swift-minutiae-project/swift-minutiae-project/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signIn(_ sender: UIButton) {
+        
+        print("Not implemented yet")
+        
+    }
+    
 }
 
