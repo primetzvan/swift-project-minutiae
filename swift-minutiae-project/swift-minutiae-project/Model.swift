563989776d0027bf9ec47a66b3bd94fabff1aaed
Vanessa Primetzhofer <vaniprimetz@gmail.com>
2021-04-09 15:46:29 -0700

    updated detailviewcontroller

---
+++ swift-minutiae-project/swift-minutiae-project/Model.swift
@@ -0,0 +1,18 @@
+//
+//  Model.swift
+//  swift-minutiae-project
+//
+//  Created by Vanessa Primetzhofer on 09.04.21.
+//
+
+import Foundation
+
+class DoorTable{
+    var doorID = 0
+    var name = ""
+    var iP = ""
+}
+
+class Model{
+    var door = [DoorTable]()
+}
